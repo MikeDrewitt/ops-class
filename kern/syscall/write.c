3fa00764c5d1c2505060b1890160b896b76023a9
Michael Drewitt <mikedrewitt@gmail.com>
2017-02-08 04:31:07 +0000

    added write description in a comment

--- kern/syscall/write.c
+++ kern/syscall/write.c
@@ -3,8 +3,21 @@
 #include <syscall.h>
 
 /*
- * Example system call: get the time of day.
+ * write writes up to buflen bytes to the file specified by fd, at the 
+ * location in the file specified by the current seek position of the 
+ * file, taking the data from the space pointed to by buf. The file must 
+ * be open for writing.
+ *
+ * The current seek position of the file is advanced by the number of 
+ * bytes written.
+ *
+ * Each write (or read) operation is atomic relative to other I/O to the same 
+ * file. Note that the kernel is not obliged to (and generally cannot) make 
+ * the write atomic with respect to other threads in the same process 
+ * accessing the I/O buffer during the write.
+ *
  */
+
 size_t
 sys_write(int fd, const void *buf, size_t nbytes)
 {
