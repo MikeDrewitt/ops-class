7b2279890ef19bf0a3bf0e93fcc6ee9863d86477
Michael Drewitt <mikedrewitt@gmail.com>
2017-03-16 02:38:30 +0000

    almost done with dup2 badcalls

--- kern/syscall/dup2.c
+++ kern/syscall/dup2.c
@@ -14,26 +14,32 @@
 int
 sys_dup2(int32_t *retval, int oldfd, int newfd)
 {
-
-//	(void)oldfd;
-
+	int err;
 	
-	// If The newfd is already open
-	lock_acquire(curproc->p_filetable[oldfd]->ft_lock);
-	if (curproc->p_filetable[newfd] != NULL) {
-		// kprintf("dup2 calling close \n");
-		sys_close(retval, newfd);
+	if (oldfd == newfd) {
+		*retval = newfd;
+		return 0;	
 	}
-	
+
+	if (oldfd < 0 || newfd < 0 || oldfd > 64 || newfd > 64 || curproc->p_filetable[oldfd] == NULL) {
+		*retval = EBADF;
+		return -1;
+	}
+
+	if (curproc->p_filetable[newfd] != NULL) {
+		err = sys_close(retval, newfd);
+		if (err) {
+			return -1;
+		}
+	}
+
+	lock_acquire(curproc->p_filetable[oldfd]->ft_lock);
+
 	curproc->p_filetable[oldfd]->ref_counter += 1;
-	
-	// kprintf("dup2 ref count increase = %d \n", curproc->p_filetable[oldfd]->ref_counter);
-	
-	curproc->p_filetable[newfd] = curproc->p_filetable[oldfd];
-	
-	 lock_release(curproc->p_filetable[oldfd]->ft_lock);
-	
+	curproc->p_filetable[newfd] = curproc->p_filetable[newfd];
+
+	lock_release(curproc->p_filetable[oldfd]->ft_lock);
+
 	*retval = newfd;
-	// kprintf("finishing dup2\n");
 	return 0;
 }
